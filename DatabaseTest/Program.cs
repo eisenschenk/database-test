1191960a0dd36a9d09bdc1acc1315e4da28ba076
Eisenschenk <o.eisenschenk@imassense.de>
2020-02-20 15:26:24 +0100

    inital commit

---
+++ DatabaseTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DatabaseTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var manager in new employeesEntities().dept_manager.Include(x => x.employees).Include(x => x.departments))
+                Console.WriteLine($"{manager.employees.last_name} {manager.from_date} {manager.to_date} {manager.departments.dept_name}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
